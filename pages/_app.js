cf2e3a9014598211ceccc71603e4020b2f61c915
Bouzir Mohamed Ali <86047475+MohamedAliBouzir@users.noreply.github.com>
2022-05-13 02:53:28 +0200

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -30,23 +30,7 @@ Router.events.on("routeChangeError", () => {
 export default class MyApp extends App {
   componentDidMount() {
     let comment = document.createComment(`
-
-=========================================================
-* Notus NextJS - v1.1.0 based on Tailwind Starter Kit by Creative Tim
-=========================================================
-
-* Product Page: https://www.creative-tim.com/product/notus-nextjs
-* Copyright 2021 Creative Tim (https://www.creative-tim.com)
-* Licensed under MIT (https://github.com/creativetimofficial/notus-nextjs/blob/main/LICENSE.md)
-
-* Tailwind Starter Kit Page: https://www.creative-tim.com/learning-lab/tailwind-starter-kit/presentation
-
-* Coded by Creative Tim
-
-=========================================================
-
-* The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
-
+TEP - THE ENGINEERS PROJECT
 `);
     document.insertBefore(comment, document.documentElement);
   }
@@ -72,7 +56,6 @@ export default class MyApp extends App {
             content="width=device-width, initial-scale=1, shrink-to-fit=no"
           />
           <title>TEP - The Engineers Project</title>
-          <script src="https://maps.googleapis.com/maps/api/js?key=YOUR_KEY_HERE"></script>
         </Head>
         <Layout>
         <RouteGuard>
