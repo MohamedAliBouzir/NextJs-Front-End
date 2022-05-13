456fa3844a001644348d7a8127af341c70a4534f
Bouzir Mohamed Ali <86047475+MohamedAliBouzir@users.noreply.github.com>
2022-05-13 02:58:25 +0200

    Update Admin.js

--- layouts/Admin.js
+++ layouts/Admin.js
@@ -3,6 +3,7 @@ import React from "react";
 // components
 
 import Sidebar from "components/Sidebar/Sidebar.js";
+import HeaderStats from "components/Headers/HeaderStats.js";
 import FooterAdmin from "components/Footers/FooterAdmin.js";
 
 export default function Admin({ children }) {
@@ -10,6 +11,7 @@ export default function Admin({ children }) {
     <>
       <Sidebar />
       <div className="relative md:ml-64 bg-blueGray-100">
+        {<HeaderStats />}
         <div className="px-4 md:px-10 mx-auto w-full -m-24">
           {children}
           <FooterAdmin />
